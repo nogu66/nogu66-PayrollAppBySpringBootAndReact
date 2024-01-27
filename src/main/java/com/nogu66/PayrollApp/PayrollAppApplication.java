ce258255d7b5b9c5ad35bfed606ce97758662965
Yuta <ezochi66@gmail.com>
2024-01-27 10:10:05 +0900

    Web画面上でのAPI情報の表示

--- src/main/java/com/nogu66/PayrollApp/PayrollAppApplication.java
+++ src/main/java/com/nogu66/PayrollApp/PayrollAppApplication.java
@@ -1,13 +1,16 @@
-package com.nogu66.PayrollApp;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class PayrollAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PayrollAppApplication.class, args);
-	}
-
-}
+//package com.nogu66.PayrollApp;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@SpringBootApplication
+//@RestController
+//public class PayrollAppApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(PayrollAppApplication.class, args);
+//	}
+//}
